c795b586fe892ca1e3013262e6a1d58f1b08db72
laerling <laerling@posteo.de>
2018-09-30 13:13:01 +0200

    test.sh: Make less files

--- test.sh
+++ test.sh
@@ -2,12 +2,13 @@
 
 rm -rf test;
 mkdir test; cd test || exit;
-for i in {0..9}; do
+for i in {0..3}; do
 	mkdir $i; cd $i;
-	for j in {0..9}; do
+	for j in {0..3}; do
 		mkdir $i_$j; cd $i_$j;
-		for k in {0..9}; do
-			dd if=/dev/urandom of=$i_$j_$k bs=1024 count=1024;
+		for k in {0..3}; do
+			dd if=/dev/urandom of="$i\"$j' $k" bs=1024 count=1024;
+			dd if=/dev/urandom of="$i-$j $k" bs=1024 count=1024;
 		done;
 		cd ..;
 	done;
