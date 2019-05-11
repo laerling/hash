b173173b1e17e097d75442cbff04cf1b7b09816a
laerling <laerling@posteo.de>
2019-05-11 22:59:53 +0200

    flatten.pl: Improve existing comments and add new ones

--- flatten.pl
+++ flatten.pl
@@ -9,29 +9,33 @@ my $loc = shift || "."; # location to traverse
 # check location
 die "Error: $loc is not a directory" unless -d $loc;
 
-# check safety
-print "WARNING: Flattening directory '$loc'. Continue? (y/N) ";
+# check user sanity
+print "WARNING: Flattening directory '$loc'. This cannot be undone. Continue? (y/N) ";
 my $ok = <>; # ask for user input
 if($ok ne "y\n"){
     print "Exiting.\n";
     exit;
 }
 
-# start traversing
+# start traversing recursively. Don't call flatten with a second argument, else
+# all files will be moved into the parent directory of $loc.
 flatten($loc);
 
+# recurses depth-first into the given directory and moves every file one
+# directory up until all files are at the upmost directory. Directories whose
+# contents must not be moved upwards (e. g. .git) are removed entirely.
 sub flatten {
 
     # initialize
     my $loc = shift || ".";
     my $migrate = shift || "";
 
-    # first pass: Recurse
+    # Recurse into directories
     opendir(my $dir, $loc);
     while(my $item = readdir $dir){
 
         # canonize $loc and build path
-        $loc = $loc . "/" unless substr($loc, -1) eq '/';
+        $loc .= "/" unless substr($loc, -1) eq '/';
         my $itempath = "$loc$item";
 
         # skip non-directories
@@ -47,13 +51,15 @@ sub flatten {
             next;
         }
 
-        # traverse and remove
+        # recurse into directory. The second argument says to move files upwards
         flatten($itempath, 1);
+
+        # remove now empty directory
         die if system("rmdir", "$itempath");
     }
     closedir($dir);
 
-    # second pass: Migrate
+    # Move files upwards
     if($migrate) {
         opendir($dir, $loc);
         while(my $item = readdir $dir){
